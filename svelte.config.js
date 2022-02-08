02022b84760cf19a8feee899f4ee6bfbdf6c9611
Someone Somewhere <someonesomewheredis@gmail.com>
2022-02-08 15:37:21 +0300

    fix: change baseURL when building for gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
+const ghpages = process.env.NODE_ENV === "ghpages";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -12,6 +14,10 @@ const config = {
 	],
 
 	kit: {
+		paths: {
+			base: ghpages ? "/muslim-projects-site" : ""
+		},
+
 		adapter: adapter()
 	}
 };
